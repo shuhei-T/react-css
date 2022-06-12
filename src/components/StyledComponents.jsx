c0ab26e46c1266db7a297d2ba41b54b22cf4a40a
shuhei-T <81573592+shuhei-T@users.noreply.github.com>
2022-06-12 21:48:49 +0900

    styled components

--- src/components/StyledComponents.jsx
+++ src/components/StyledComponents.jsx
@@ -1,4 +1,15 @@
-.container {
+import styled from "styled-components";
+
+export const StyledComponents = () => {
+  return (
+    <SContainer>
+      <STitle>- styled components -</STitle>
+      <SButton>FIGHT!!</SButton>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
   border: solid 2px #392eff;
   border-radius: 20px;
   padding: 8px;
@@ -6,12 +17,12 @@
   display: flex;
   justify-content: space-around;
   align-items: center;
-}
-.title {
+`;
+const STitle = styled.p`
   margin: 0;
   color: #3d84a8;
-}
-.button {
+`;
+const SButton = styled.button`
   background-color: #abedd8;
   border: none;
   padding: 8px;
@@ -21,4 +32,4 @@
     color: #fff;
     cursor: pointer;
   }
-}
+`;
